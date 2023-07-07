a280ab3d927c1033e523ddc71ec66413d47a0c7f
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-07-07 21:39:05 +0300

    add less

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import Container from './components/Container/Container'
 import ProductsList from './components/ProductsList/ProductsList'
-import ProductsList2 from './components/ProductsList2/ProductsList2'
 import data from './data.json'
-import data2 from './data2.json'
 
 const App = () => {
 	return (
-		<>
-			{/* {ProductsList({ params1: data, params2: data2 })} */}
-			<Container>
-				<ProductsList products={data} />
-			</Container>
-			<Container>
-				<ProductsList2 products={data2} />
-			</Container>
-		</>
+		<Container>
+			<ProductsList products={data} />
+		</Container>
 	)
 }
 
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import styled from './Container.module.css'
 const Container = ({ children }) => {
 	return (
-		<div>
+		<div className={styled.container}>
+			{/* <div className='container'> */}
 			<div>{children}</div>
 		</div>
 	)
