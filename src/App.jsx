ec3ce61b4c48ac18650a1f87b3e0b16506a85672
anastasiia.chikrizova@yassir.com <aliev.lomach.maxim@gmail.com>
2023-07-26 22:05:45 +0300

    add less

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import ProductsList from './components/ProductsList/ProductsList'
+// import ProductsList from './components/ProductsList/ProductsList'
 import Header from './components/Header/Header'
 import Modal from './components/Modal/Modal'
-import { Component } from 'react'
 
-class App extends Component {
-	state = {
-		isShowModal: false,
-	}
-	toggleModal = () => {
-		this.setState((prev) => ({ isShowModal: !prev.isShowModal }))
+import { useState } from 'react'
+
+const App = () => {
+	const [isShowModal, setIsShowModal] = useState(false)
+
+	const toggleModal = () => {
+		setIsShowModal((prev) => {
+			return !prev
+		})
 	}
 
-	render() {
-		return (
-			<div className='container'>
-				<Header open={this.toggleModal} />
-				<ProductsList />
-				{this.state.isShowModal && (
-					<Modal close={this.toggleModal}>MODAL</Modal>
-				)}
-			</div>
-		)
-	}
+	return (
+		<div className='container'>
+			<Header open={toggleModal} />
+			{/* <ProductsList /> */}
+			{/* <Counter /> */}
+			{isShowModal && <Modal close={toggleModal}>MODAL</Modal>}
+		</div>
+	)
 }
 
-// if (showModal){
-// 	return Modal()
-// }
-// const App = () => {
-// 	return (
-// 		<div className='container'>
-// 			<Header />
-// 			<Counter />
-// 			<Modal>MODAL</Modal>
-// 		</div>
-// 	)
-// }
-
 export default App
+
+// class App extends Component {
+// state = {
+// 	isShowModal: false,
+// }
+// 	toggleModal = () => {
+// this.setState((prev) => ({ isShowModal: !prev.isShowModal }))
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className='container'>
+// 				<Header open={this.toggleModal} />
+// 				<ProductsList />
+// 				{this.state.isShowModal && (
+// 					<Modal close={this.toggleModal}>MODAL</Modal>
+// 				)}
+// 			</div>
+// 		)
+// 	}
+// }
+// export default App
